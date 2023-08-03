c085b307619869beebb882b0caa6a7e37f6e3f64
ishika prasad <dp878917@gmail.com>
2023-08-03 14:24:55 +0530

    commit

--- script.js
+++ script.js
@@ -13,12 +13,37 @@ function stop(){
     timer=false;
 }
 function reset(){
-
+    timer=false;
+    hr=0;
+    min=0;
+    sec=0;
+    count=0;
+    document.getElementById("hr").innerHTML=hr;
+    document.getElementById("min").innerHTML=min;
+    document.getElementById("sec").innerHTML=sec;
+    document.getElementById("count").innerHTML=count;
 }
 function stopwatch(){
-    if(timer=="true"){
+    if(timer==true){
         count=count+1;
+        if(count==100){
+            sec=sec+1;
+            count=0;
+        }
+        if(sec==60){
+            min=min+1;
+            sec=0;
+        } if(min==60){
+            hr=hr+1;
+            min=0;
+            sec=0;
+        }
+        document.getElementById("hr").innerHTML=hr;
+        document.getElementById("min").innerHTML=min;
+        document.getElementById("sec").innerHTML=sec;
         document.getElementById("count").innerHTML=count;
+        
+
 
         setTimeout("stopwatch()", 10);
     }
